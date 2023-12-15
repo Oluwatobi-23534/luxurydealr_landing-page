f37ab6b610080f88f180ee01a8664bf0081ae5ce
Oluwatobi-23534 <oluwatobi23534@gmail.com>
2023-12-15 14:32:03 +0100

    added instructional/promo video to VRproducts page

--- src/pages/VRProductDetails.jsx
+++ src/pages/VRProductDetails.jsx
@@ -75,13 +75,23 @@ const vrHeadsets = [
   },
 ];
 
-
 const VRProductDetails = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-blue-50 pt-28">
       <h1 className="text-4xl font-bold mt-4 border-4 border-blue-500 rounded-lg bg-gradient-to-r from-blue-500 to-blue-700 text-white p-2 shadow-lg">
         VR Headsets
       </h1>
+      <iframe
+        title="Promo Video"
+        frameBorder="0"
+        width="100%"
+        height="100%"
+        src="https://promo.com/embed/6579f3c85f5db859c2745b22?ratioAspect=square&type=preview"
+        allowFullScreen={true}
+        className="my-4 w-1/2 sm:w-3/8 md:w-1/4 lg:w-1/3 xl:w-1/4 mx-auto rounded-lg shadow-lg overflow-hidden p-2 sm:p-4"
+        style={{ aspectRatio: "1" }}
+      ></iframe>
+
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-6 w-full max-w-8xl mb-6 p-12">
         {vrHeadsets.map((headset, index) => (
           <div
@@ -110,8 +120,5 @@ const VRProductDetails = () => {
     </div>
   );
 };
-
-
-
 
 export default VRProductDetails;
